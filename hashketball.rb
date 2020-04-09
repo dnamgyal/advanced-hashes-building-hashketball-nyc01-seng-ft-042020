97124b875c7209634e5927daee60b449a78cdf4c
Don Namgyal <dhondup.namgyal@gmail.com>
2020-04-09 22:25:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -27,26 +27,26 @@ def game_hash
           slam_dunks: 7
         }
         {
-          player_name: "Alan Anderson",
-          number: 0,
-          shoe: 0,
-          points:
-          rebounds:
-          assists:
-          steals:
-          blocks:
-          slam_dunks:
+          player_name: "Brook Lopez",
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
         }
         {
-          player_name: "Alan Anderson",
-          number: 0,
-          shoe: 0,
-          points:
-          rebounds:
-          assists:
-          steals:
-          blocks:
-          slam_dunks:
+          player_name: "Mason Plumlee",
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 11,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
         }
         {
           player_name: "Alan Anderson",
