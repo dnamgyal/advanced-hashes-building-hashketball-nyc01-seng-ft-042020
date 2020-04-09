208deef8420996afcd9d796ed72f785428733ccd
Don Namgyal <dhondup.namgyal@gmail.com>
2020-04-09 22:21:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,20 @@
 # Write your code here!
-
-
 def game_hash
   new_hash = {
-    home: {team_name: "Brooklyn Nets", colors: ["Black", "White"], players: {}},
+    home: {team_name: "Brooklyn Nets", colors: ["Black", "White"], 
+      players: [
+        {
+          player_name: "",
+          number: "",
+          shoe: "",
+          points: "",
+          rebounds: "",
+          assists: "",
+          steals: "", 
+          blocks: "",
+          slam_dunks: ""
+        },
+      ],
     away: {team_name: "Charlotte Hornets", colors: ["Turquoise", "Purple"], players: {}}
   }
   new_hash
