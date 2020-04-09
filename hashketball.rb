a9b3b9e3cd1c1b0e717bce3cf499728dcf6584d1
Don Namgyal <dhondup.namgyal@gmail.com>
2020-04-09 22:17:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,10 @@
 # Write your code here!
+
+
+def game_hash
+  new_hash = {
+    home: {team_name: {}, colors: {}, players: {}},
+    away: {team_name: {}, colors: {}, players: {}}
+  }
+  new_hash
+end
